706983bcb63ad81f4b9737b245100e6c7ba029af
AJVelezRueda <anavelezrueda@gmail.com>
2021-06-18 23:37:39 -0300

    styles

--- index.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    var sticky = $('.sticky'),
-        scroll = $(window).scrollTop();
-  
-    if (scroll >= 100) sticky.addClass('fixed');
-    else sticky.removeClass('fixed');
-  });
